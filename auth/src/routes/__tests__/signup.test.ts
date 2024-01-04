35f4026457185285a703766e7dc855d1a9abb436
jewtechx <jklarbi_danquah@st.ug.edu.gh>
2024-01-04 14:12:32 -0800

    created sign up branch

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -16,13 +16,10 @@ afterAll(() => {
 it('should return 405 for non post request sign up route', async () => {});
 
 it('should return 422 if the email is not valid', async () => {
-  await request(app)
-  .post('/api/auth/signup')
-  .send({})
-  .expect(422)
+  await request(app).post('/api/auth/signup').send({}).expect(422);
 
   await request(app)
-  .post('/api/auth/signup')
-  .send({email:"invalidemail"})
-  .expect(422)
+    .post('/api/auth/signup')
+    .send({ email: 'invalidemail' })
+    .expect(422);
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,17 +1,22 @@
-import express, { Request, Response } from 'express'
-import {body,validationResult} from 'express-validator'
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
-const signUpRouter = express.Router()
+interface bodyInterface {
+  email: string;
+}
 
-signUpRouter.post('/api/auth/signup',
-[body('email').isEmail().withMessage('Email must be in a valid format')] 
-,(req:Request,res:Response) => {
-    
-    const errors = validationResult(req)
+const signUpRouter = express.Router();
 
-    if(!errors.isEmpty()){
-        res.status(422).send({})
+signUpRouter.post(
+  '/api/auth/signup',
+  [body('email').isEmail().withMessage('Email must be in a valid format')],
+  (req: Request<{}, {}, bodyInterface>, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).send({});
     }
-})
+  },
+);
 
-export default signUpRouter
+export default signUpRouter;
