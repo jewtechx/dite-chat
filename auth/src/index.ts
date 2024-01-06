ec5fde58b878635216358e76d08c00d690c303b5
jewtechx <jklarbi_danquah@st.ug.edu.gh>
2024-01-06 14:22:24 -0800

    refactor(auth) : non post request test handlers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,4 +2,4 @@ import { app } from './app';
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Listening on Port ${PORT}`));
